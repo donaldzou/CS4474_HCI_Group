c9686e1ed4d7ecc65417c2ab03224a346f04128a
Donald Zou <donaldzou@live.hk>
2024-03-20 01:57:03 -0400

    Update app base

--- WebApp/vite.config.js
+++ WebApp/vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: "/CS4474_HCI_Group/dist/"
+  base: "/CS4474_HCI_Group/WebApp/dist/"
 })
